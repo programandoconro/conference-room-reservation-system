602fc79b3fbec6039f56ee01c8202ff551fbbbe7
rodrigo <ro@PC-131noMacBook-Air.local>
2022-01-28 15:55:06 +0900

    japanese calendar

---
+++ pages/reservation.tsx
@@ -0,0 +1,15 @@
+import Calendar from "../components/calendar";
+
+const Reservation = () => {
+  return (
+    <div>
+      <header>
+        <h1>Reservations:</h1>
+      </header>
+
+      <Calendar />
+    </div>
+  );
+};
+
+export default Reservation;
