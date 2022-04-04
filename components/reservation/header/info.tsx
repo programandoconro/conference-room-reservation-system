45677894ac47f5180d0c9e93040113d3dba5de9c
Ro <102496363+rodrigoamericodiaz@users.noreply.github.com>
2022-04-04 15:17:30 +0900

    fixed delete right reservation

--- components/reservation/header/info.tsx
+++ components/reservation/header/info.tsx
@@ -25,53 +25,56 @@ const Info = () => {
   };
   return (
     <div className="flex flex-col w-1/3 bg-slate-100 p-1 mt-1 overflow-y-auto">
-      {reservations.map((res, key) => {
-        const timeStart = new Date(res.start).toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit",
-        });
-        const timeEnd = new Date(res.end).toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit",
-        });
-        return (
-          <div key={key} className="">
-            {user.company === res.company &&
-              (isBefore(new Date(date), new Date(res.date)) ||
-                res.date === date) && (
-                <Card
-                  className="flex min-w-full mt-2 w-8/12 "
-                  style={{
-                    backgroundColor: date === res.date ? "yellow" : "white",
-                  }}
-                >
-                  <CardContent>
-                    <Typography
-                      style={{ fontWeight: "lighter", fontSize: "12px" }}
-                    >
-                      {res.date} {timeStart} - {timeEnd}
-                      {res.room}
-                      <button>
-                        <h5 className="pl-2 text-blue-700 text-xs hover:text-blue-500">
-                          編集
-                        </h5>
-                      </button>
-                      <button
-                        onClick={() => {
-                          handleDelete(key);
-                        }}
+      {reservations
+        .slice(0)
+        .reverse()
+        .map((res, key) => {
+          const timeStart = new Date(res.start).toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          });
+          const timeEnd = new Date(res.end).toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          });
+          return (
+            <div key={key} className="">
+              {user.company === res.company &&
+                (isBefore(new Date(date), new Date(res.date)) ||
+                  res.date === date) && (
+                  <Card
+                    className="flex min-w-full mt-2 w-8/12 "
+                    style={{
+                      backgroundColor: date === res.date ? "yellow" : "white",
+                    }}
+                  >
+                    <CardContent>
+                      <Typography
+                        style={{ fontWeight: "lighter", fontSize: "12px" }}
                       >
-                        <h5 className="pl-2 text-blue-700 text-xs hover:text-blue-500">
-                          削除
-                        </h5>
-                      </button>
-                    </Typography>
-                  </CardContent>
-                </Card>
-              )}
-          </div>
-        );
-      })}
+                        {res.date} {timeStart} - {timeEnd}
+                        {res.room}
+                        <button>
+                          <h5 className="pl-2 text-blue-700 text-xs hover:text-blue-500">
+                            編集
+                          </h5>
+                        </button>
+                        <button
+                          onClick={() => {
+                            handleDelete(key);
+                          }}
+                        >
+                          <h5 className="pl-2 text-blue-700 text-xs hover:text-blue-500">
+                            削除
+                          </h5>
+                        </button>
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                )}
+            </div>
+          );
+        })}
     </div>
   );
 };
